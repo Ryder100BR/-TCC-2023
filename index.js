3bdfa90cabbc6e77d3bcd9282919c4534ed90b83
Ryder <110050673+Ryder100BR@users.noreply.github.com>
2023-08-22 18:28:22 +0000

    modified:   index.js 	new file:   public/index.html 	new file:   src/config/auth.js 	modified:   src/controllers/projetos/index.js 	modified:   src/controllers/usuarios/index.js 	new file:   src/controllers/usuariosprojetos/index.js 	modified:   src/routes/index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {server: api} = require('./src/api/index') 
 const app = express()
 const cors = require ('cors')
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
 const {routes} = require("./src/routes/index");
 
