a17921dc2796d23c56dc308c7a17ae6a219564d7
Ryder <110050673+Ryder100BR@users.noreply.github.com>
2023-11-16 17:02:29 +0000

    g

--- src/controllers/usuarios/index.js
+++ src/controllers/usuarios/index.js
@@ -28,6 +28,12 @@ class Usuarios {
       usu_email: string()
         .email("Entre com um e-mail válido")
         .required("Entre com o e-mail"),
+      usu_senha: string()
+      .required("Entre com a senha")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/,
+        "A senha precisa ter no mínimo 6 caracteres, sendo: uma maiúscula, uma minúscula, um número e um caracter especial"
+      ),
       usu_nivel: mixed(["admin", "comum"], "Tipo de usuário incorreto")
     });
 
